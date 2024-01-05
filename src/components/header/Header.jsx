dfbe7f3cd1bc55e34b1c5b66a1391d23bf4b206d
lavanyaPulugu <lavanya415@gmail.com>
2024-01-05 20:32:55 +0530

    complete nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,21 +6,23 @@ import Social from "./Social";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container header-container">
-        <h5>Hello I'm</h5>
-        <h1>Lavanya Pulugu</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
-        <CTA />
-        <Social />
-        <div className="me">
-          <img src={ME} alt="" />
+    <section id="header">
+      <header>
+        <div className="container header-container">
+          <h5>Hello I'm</h5>
+          <h1>Lavanya Pulugu</h1>
+          <h5 className="text-light">Fullstack Developer</h5>
+          <CTA />
+          <Social />
+          <div className="me">
+            <img src={ME} alt="" />
+          </div>
+          <a href="#contact" className="scroll_down">
+            Scroll Down
+          </a>
         </div>
-        <a href="#contact" className="scroll_down">
-          Scroll Down
-        </a>
-      </div>
-    </header>
+      </header>
+    </section>
   );
 };
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./nav.css";
+import { FaHome } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageDetail } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#home");
+  return (
+    <nav>
+      <a
+        href="#header"
+        onClick={() => setActiveNav("#header")}
+        className={activeNav === "#header" ? "active" : ""}
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FaRegUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
